2193834c26e60e33d759bbfbad2cda977651dd24
ilnarkin861 <40397175+ilnarkin861@users.noreply.github.com>
2022-01-12 18:31:02 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: "https://localhost:5001",
   authApiUrl: "https://localhost:5002",
-  googleProviderId: "461964907385-49u6m895r6q1rs8mk6a3qrgt8c5loinq.apps.googleusercontent.com"
+  googleProviderId: "your_id"
 };
